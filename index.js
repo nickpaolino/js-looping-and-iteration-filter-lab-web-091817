61f57fbfaeff38087b9d2e78b269dfbb3ee16213
Nick Paolino <github email address>
2017-10-30 13:25:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution in this file
+function findMatching(array, name){
+  const newArray = array.filter(
+    function(n) {return n.toUpperCase() === name.toUpperCase()}
+  )
+  return newArray
+}
+
+function fuzzyMatch(array, letters){
+  let nameLength = letters.length;
+  const newArray = array.filter(
+    function(element) {
+      return element.slice(0, nameLength) === letters;
+    }
+  )
+  return newArray;
+}
+
+function matchName(obj, word){
+  return obj.filter(
+    function(object){return object.name === word}
+  )
+}
